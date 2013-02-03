88d516a28e9c46c5b7a07dfaa62bed2d8b0bb4be
gmlunardi@gmail.com <gmlunardi@gmail.com@f58eb68b-d891-9c6f-ca9f-c293a8d43889>
2013-02-03 16:40:59 +0000

    Adicionado form de cadastro de aeronaves, mas sem o controller. Revisar controller de Alunos, pois pode ser preciso movelo para a view JFramePrincipal. Adicionado um componente JCalendar (externo) para a selecao mais intuitiva de datas. Adicionar essa dependencia (que estah nesse commit) para rodar a aplicacao legal.

--- EscolaAviacao/src/view/JFramePrincipal.java
+++ EscolaAviacao/src/view/JFramePrincipal.java
@@ -30,6 +30,7 @@ public class JFramePrincipal extends javax.swing.JFrame {
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu2 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
         jMenu3 = new javax.swing.JMenu();
         jMenu4 = new javax.swing.JMenu();
 
@@ -48,6 +49,14 @@ public class JFramePrincipal extends javax.swing.JFrame {
             }
         });
         jMenu2.add(jMenuItem1);
+
+        jMenuItem2.setText("Aeronaves");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem2);
 
         jMenuBar1.add(jMenu2);
 
@@ -78,6 +87,11 @@ public class JFramePrincipal extends javax.swing.JFrame {
         cadAlunos.setVisible(true);
     }//GEN-LAST:event_jMenuItem1ActionPerformed
 
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        JFrameCadAvioes cadAvioes = new JFrameCadAvioes();
+        cadAvioes.setVisible(true);
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
@@ -85,5 +99,6 @@ public class JFramePrincipal extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu4;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
     // End of variables declaration//GEN-END:variables
 }
